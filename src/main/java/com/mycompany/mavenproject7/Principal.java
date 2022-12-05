8e2f1d6e3c7c4c19561f541d777f948e52596bf7
amawt <amawt@LAPTOP-CESHFSKU>
2022-12-05 07:01:07 -0500

    Terminado 1

---
+++ src/main/java/com/mycompany/mavenproject7/Principal.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject7;
+
+/**
+ *
+ * @author amawt
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+    /*var ventana = new Inscribete("Registro de inscripcion");*/
+
+        var datoPersonal = new DatoPersonal("Datos Personales");
+        var crearGrupo = new CrearGrupo("Crear Grupo");
+        var infoCarrera = new InformacionCarrera("Informacion de la carrera");
+
+
+        var inscribete1 = new Inscribete("Inscribete");
+        var ventana1 = new Final ("Ventana");
+        
+    }   
+    
+}
